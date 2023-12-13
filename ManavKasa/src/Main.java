9f6ab82515109710461447d652cde35dfe17877a
denizyaman <denzzyamn@gmail.com>
2023-12-13 22:57:31 +0300

    Manav fiyat hesaplaması

--- ManavKasa/src/Main.java
+++ ManavKasa/src/Main.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double armut,elma,domates,muz,patlican;
+        double armutKg,elmaKg,domatesKg,muzKg,patlicanKg,armutTl=2.14,elmaTl=3.67,domatesTl=1.11,muzTl=0.95,patlicanTl=5.00,total;
         Scanner inp=new Scanner(System.in);
-        System.out.print("Armut kaç kilo?");
-        System.out.print("Armut kaç kilo?");
-        System.out.print("Armut kaç kilo?");
-        System.out.print("Armut kaç kilo?");
-        System.out.print("Armut kaç kilo?");
+        System.out.print( "Armutun kilosu " + armutTl+" Armut kaç kilo? ");
+        armutKg= inp.nextDouble();
+        System.out.print(" Elmanın kilosu "+ elmaTl +" Elma kaç kilo?");
+        elmaKg= inp.nextDouble();
+        System.out.print(" Domatesin kilosu " + domatesTl +" Domates kaç kilo?");
+        domatesKg= inp.nextDouble();
+        System.out.print(" Muzun kilosu "+ muzTl + " Muz kaç kilo?");
+        muzKg= inp.nextDouble();
+        System.out.print(" Patlıcanın kilosu "+ patlicanTl + " Patlıcan kaç kilo?");
+        patlicanKg= inp.nextDouble();
+        if ((armutKg>=0 && elmaKg>=0 && domatesKg>=0 && muzKg>=0 && patlicanKg>=0)) {
+            total = (armutTl * armutKg) + (elmaTl * elmaKg) + (domatesTl * domatesKg) + (muzTl * muzKg) + (patlicanTl * patlicanKg);
+            System.out.println(total);
+        }else{
+            System.out.println("Pozitif değer giriniz.");
+        }
     }
 }
