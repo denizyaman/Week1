e896c3ddafef858c8d47c5ac89ea07ea5a5c396d
denizyaman <denzzyamn@gmail.com>
2023-12-13 22:06:24 +0300

    Manav kasa uygulaması eklendi

---
+++ ManavKasa/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double armut,elma,domates,muz,patlican;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Armut kaç kilo?");
+        System.out.print("Armut kaç kilo?");
+        System.out.print("Armut kaç kilo?");
+        System.out.print("Armut kaç kilo?");
+        System.out.print("Armut kaç kilo?");
+    }
+}
